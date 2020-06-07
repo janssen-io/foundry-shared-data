1745d8f9bc18c5d4d6cad41b3f5c10ec34579937
Stan Janssen <mail@janssen.io>
2020-06-07 22:32:40 +0200

    Fancier update-tokens; TODO: add to compendium

--- macros/update-tokens.js
+++ macros/update-tokens.js
@@ -1,14 +1,23 @@
-// Example: Enlarge token
+const isNpc = token => token.actor.data.type == "npc";
+const hasName = name => token => token.data.name.toLowerCase()
+	.includes(name.toLowerCase());
 
-const partOfName = "Blight";
+const isHidden = token => token.data.hidden;
+
+const not = f => token => !f(token);
+const and = function() {
+  let args = [...arguments];
+	return token => args.every(f => f(token));
+}
+
 const updates = canvas.tokens.placeables
-    .filter(t => t.name.includes(partOfName))
-    .map(t => {
-        return {
-              _id: t.id,
-              width: t.data.width * 2,
-              height: t.data.height * 2
-        }
+    .filter(and(isNpc, not(isHidden)))
+    .map(token => {
+      return {
+        _id: token.id,
+        displayName: CONST.TOKEN_DISPLAY_MODES.HOVER,
+        displayBars: CONST.TOKEN_DISPLAY_MODES.HOVER
+      }
     });
 
 canvas.tokens.updateMany(updates);
